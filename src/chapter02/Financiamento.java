a3eaed8fea499dc4c5b0cebbe54c3b7c62a9c32b
philippematias <felipmatias88@gmail.com>
2024-06-10 19:36:30 -0300

    projetinho atualizdo

--- src/chapter02/Financiamento.java
+++ src/chapter02/Financiamento.java
@@ -10,46 +10,94 @@ public class Financiamento
 {
     //-----------------------------------------------------
     Scanner m_scan = new Scanner(System.in);
+    boolean m_running = true;
 
     //-----------------------------------------------------
     public static void main(String[] args)
     {
         var fn = new Financiamento();
-        fn.mainMenu();
+        fn.loop();
     }
 
+    //-----------------------------------------------------
+    public void loop()
+    {
+        while (m_running)
+            this.mainMenu();
+    }
 
     //-----------------------------------------------------
     public void mainMenu()
     {
-        boolean run = true;
+        try {
+            this.logo();
 
-        while (run)
-        {
-            try {
-                this.logo();
+            System.out.println("\n| escolha uma opção:\n");
+            System.out.println("1. Aplicação");
+            System.out.println("2. Financiamento");
+            System.out.println("3. Valor Futuro");
+            System.out.println("4. Correção de Valores");
+            System.out.println("5. Sair");
 
-                System.out.println("| escolha uma opção:\n");
-                System.out.println("1. Aplicação");
-                System.out.println("2. Financiamento");
-                System.out.println("3. Valor Futuro");
-                System.out.println("4. Correção de Valores");
-                System.out.println("5. Sair");
-
-                System.out.print(">> ")
-                int opt = m_scan.nextInt();
-                switch(opt)
-                {
-                    case 1: break;
-                    case 2: break;
-                    case 3: break;
-                    case 4: break;
-                    case 5: run = false; break;
-                    default: throw new Exception("");
-                }
-            } catch (Exception e) {
-                this.errorMsg();
+            System.out.print("\n>> ");
+            int opt = m_scan.nextInt();
+            switch(opt)
+            {
+                case 1: this.annuityMenu(true); break;
+                case 2: break;
+                case 3: break;
+                case 4: break;
+                case 5: m_running = false; break;
+                default: throw new Exception("");
             }
+        } catch (Exception e) {
+            this.errorMsg("");
+        }
+    }
+
+    //-----------------------------------------------------
+    private void annuityMenu(boolean shwLogo)
+    {
+        if (shwLogo) this.logo();
+
+        try
+        {
+            System.out.println("\n*-------------------------------------------*");
+
+            System.out.print("| digite o valor a ser depositado: ");
+            double value = m_scan.nextDouble();
+            if (value <= 0) throw new Exception("| valor precisa ser positivo!");
+
+            System.out.print("| digite a quantidde de meses: ");
+            int time = m_scan.nextInt();
+            if (time <= 0) throw new Exception("| valor de meses invalido!");
+
+            System.out.print("| entre com a taxa de juros mensal[em %]: ");
+            double rate = (m_scan.nextDouble()/100.0);
+            if (rate <= 0) throw new Exception("| taxa de juros precisa ser positiva!");
+
+            double ac = Financiamento.annuity(value, time, rate);
+            System.out.printf("| valor acumulado + juros: %.2f\n", ac);
+
+            while (true)
+            {
+                System.out.print("\n| desejar fazer outra conta?[S/N]: ");
+                char ans = m_scan.next().charAt(0);
+                ans = Character.toUpperCase(ans);
+                if ((ans != 'S') && (ans != 'N')) {
+                    System.out.println("| opçãoin válida!");
+                    this.waitKey();
+                    continue;
+                }
+
+                if (ans == 'S') annuityMenu(false);
+                break;
+            }
+        }
+        catch (Exception e)
+        {
+            this.errorMsg(e.getMessage());
+            this.annuityMenu(false);
         }
     }
 
@@ -63,9 +111,14 @@ public class Financiamento
     }
 
     //-----------------------------------------------------
-    private void errorMsg()
+    private void errorMsg(String text)
     {
-        System.out.println("| Entrada inválida!");
+        String msg = "| Entrada inválida!";
+
+        if (text != "")
+            msg = text;
+
+        System.out.println(msg);
         this.waitKey();
     }
 
@@ -80,7 +133,7 @@ public class Financiamento
     //-----------------------------------------------------
     private void waitKey()
     {
-        var k = m_scan.nextLine();
+        new Scanner(System.in).nextLine();
     }
 
     //-----------------------------------------------------
